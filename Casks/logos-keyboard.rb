979938344db0661beb5fd0dac42bfbfdca321bcf
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2026-03-05 08:51:50 -0800

    fix: bad url

--- Casks/logos-keyboard.rb
+++ Casks/logos-keyboard.rb
@@ -2,7 +2,7 @@ cask "logos-keyboard" do
   version "0.3"
   sha256 "22eee79ff1ba3822d1d78f5c45c88eb1fa30a03c466b24074c1afb4109f5876a"
 
-  url "https://github.com/jazzz/logos-keyboard/releases/download/v0.1/LogosKeyboard.bundle.zip"
+  url "https://github.com/jazzz/logos-keyboard/releases/download/v0.3/LogosKeyboard.bundle.zip"
   name "Logos Keyboard"
   desc "Logos Keyboard bundle"
   homepage "https://github.com/jazzz/logos-keyboard"
